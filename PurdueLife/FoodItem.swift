c91b44a7b4898ec1ce0976cd9594db4e2d68acd4
Youssef <yelabd@purdue.edu>
2017-03-28 19:17:28 -0400

    first commit

---
+++ PurdueLife/FoodItem.swift
@@ -0,0 +1,13 @@
+//
+//  FoodItem.swift
+//  PurdueLife
+//
+//  Created by Youssef Elabd on 3/28/17.
+//  Copyright © 2017 Youssef Elabd. All rights reserved.
+//
+
+import UIKit
+
+class FoodItem: NSObject {
+
+}
