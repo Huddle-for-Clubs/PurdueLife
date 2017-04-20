c0540aa58dca00e156b53d5720daaab82204098e
Youssef <yelabd@purdue.edu>
2017-04-20 19:27:08 -0400

    Attempt

---
+++ PurdueLife/FavoriteDetailCell.swift
@@ -0,0 +1,24 @@
+//
+//  FavoriteDetailCell.swift
+//  PurdueLife
+//
+//  Created by Youssef Elabd on 4/18/17.
+//  Copyright © 2017 Youssef Elabd. All rights reserved.
+//
+
+import UIKit
+
+class FavoriteDetailCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ PurdueLife/ScheduleInfo.swift
@@ -0,0 +1,13 @@
+//
+//  ScheduleInfo.swift
+//  PurdueLife
+//
+//  Created by Youssef Elabd on 4/18/17.
+//  Copyright © 2017 Youssef Elabd. All rights reserved.
+//
+
+import UIKit
+
+class ScheduleInfo: NSObject {
+
+}
